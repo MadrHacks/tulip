ba58bae1329987e87951822b961158599fa2e6a6
rw-r-r-0644 <rw-r-r-0644@protonmail.com>
2025-06-20 02:21:58 +0200

    Add support for non pcap-over-ip operation to Suricata container

--- services/suricata/run.sh
+++ services/suricata/run.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env sh
 
 SURICATA_PARAMS="-c /etc/suricata/suricata.yaml \
-  -r /dev/stdin
   --pcap-file-continuous \
   --set runmode=single \
   --set outputs.0.fast.enabled=false \
   --set outputs.1.eve-log.enabled=true \
-  --set outputs.1.eve-log.filetype=redis
+  --set outputs.1.eve-log.filetype=redis \
   --set outputs.1.eve-log.redis.server=${REDIS_HOST} \
   --set outputs.1.eve-log.redis.port=${REDIS_PORT} \
   --set outputs.7.stats.enabled=false"
 
 set -x
 
-socat tcp:${PCAP_OVER_IP} - | suricata ${SURICATA_PARAMS} -v
+if [ -n "$PCAP_OVER_IP" ]; then
+  socat tcp:${PCAP_OVER_IP} - | suricata ${SURICATA_PARAMS} -r /dev/stdin -v
+else
+  suricata ${SURICATA_PARAMS} -r ${TULIP_TRAFFIC_DIR} -v
+fi
